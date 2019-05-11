0d0779a4f9b746b1d07aa2a855b3630e4fe151b0
RMonteR <talktotheowl@inventati.org>
2019-05-11 09:01:52 +0100

    update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  get 'quotes/index'
+
+  root 'quotes#index'
 
   resources :quotes
 
-  root 'quotes#index'
+  get 'quotes/index', to 'quotes#index'
+  get 'quotes', to 'quotes#index'
+  post 'quotes', to 'quotes#create'
+  get 'quotes/new', to 'quotes#new'
+  get 'quotes/:id/edit', to 'quotes#edit'
+  get 'quotes/:id', to 'quotes#show'
+  patch 'quotes/:id', to 'quotes#update'
+  put 'quotes/:id', to 'quotes#update'
+  delete 'quotes/:id' to 'quotes#destroy'
+
 end
