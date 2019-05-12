d50ec30c9c124875673eb43554546731d8f6cb74
RMonteR <talktotheowl@inventati.org>
2019-05-12 13:00:12 +0100

    quotes to quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,7 @@
 class QuotesController < ApplicationController
   def index
     # Not using Quote.limit(1).order("RANDOM()") because of a deprecation warning for Ruby 6.0.
-    @quotes = Quote.offset(rand(Quote.count)).first
+    @quote = Quote.offset(rand(Quote.count)).first
   end
 
   def show
