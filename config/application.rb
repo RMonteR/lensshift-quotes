2446eebee164ea0bbc6c45b177b29d6e275ce714
RMonteR <talktotheowl@inventati.org>
2019-05-12 13:15:43 +0100

    precompile assets

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.serve_static_assets = true
+
 module LensshiftQuotes
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
